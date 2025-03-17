a85d9ceee1148c7f0f328fcf56cf8312efc5fd68
unknown <coleyf@uninorte.edu.co>
2025-03-17 15:45:37 -0500

    Feat: Added Dog class

---
+++ src/core/Dog.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author HP
+ */
+public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" + "name=" + name + '}';
+    }
+    
+}
